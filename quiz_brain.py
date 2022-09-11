350575b7cb322cdad6747504d8fe69f97d9aec80
matusoff <94809104+matusoff@users.noreply.github.com>
2022-09-11 17:51:23 -0400

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -15,26 +15,14 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-
-        #to remove quotes from API Trivia Quiz questions, use html.unescape method
         q_text = html.unescape(self.current_question.text)
-        
-        #new update: 
-        # to get questions from API to our UI let's return the string f"Q.{self.question number}:{q_text}"
         return f"Q.{self.question_number}: {q_text}"
-        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        #self.check_answer(user_answer)
+        
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            #print("You got it right!")
             return True
         else:
-            #print("That's wrong.")
             return False
-        
-
-        # print(f"Your current score is: {self.score}/{self.question_number}")
-        # print("\n")
